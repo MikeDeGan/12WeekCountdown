2238b2cc0071c58ffebc4ae9da58065cc82e898b
Mike DeGan <mikedegan@gmail.com>
2024-03-03 20:17:52 -0500

    got to work out some date calc issues

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
   const second = 1000,
     minute = second * 60,
     hour = minute * 60,
-    day = hour * 24;
+    day = hour * 24,
+    week = day * 7;
+  const periodStart = new Date("2024-03-02T00:00:00Z").getTime();
+  const periodEnd = new Date(periodStart + 12 * week + 2 * day);
+  const countDown = new Date(periodEnd).getTime();
 
   //I'm adding this section so I don't have to keep updating this pen every year :-)
   //remove this if you don't need it
@@ -20,29 +24,26 @@
   }
   //end
 
-  const countDown = new Date(birthday).getTime(),
-    x = setInterval(function () {
-      const now = new Date().getTime(),
-        distance = countDown - now;
+  const x = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = periodEnd - now;
+    const daysRemaining = Math.floor(distance / day);
+    const weeksRemaining = Math.floor(daysRemaining / 7);
+    const currentWeek = 12 - weeksRemaining;
 
-      (document.getElementById("days").innerText = Math.floor(distance / day)),
-        (document.getElementById("hours").innerText = Math.floor(
-          (distance % day) / hour
-        )),
-        (document.getElementById("minutes").innerText = Math.floor(
-          (distance % hour) / minute
-        )),
-        (document.getElementById("seconds").innerText = Math.floor(
-          (distance % minute) / second
-        ));
+    (document.getElementById("headline").innerText = periodEnd),
+      (document.getElementById("currentWeek").innerText =
+        Math.ceil(currentWeek)),
+      (document.getElementById("weeksRemaining").innerText = weeksRemaining),
+      (document.getElementById("days").innerText = daysRemaining);
 
-      //do something later when date is reached
-      if (distance < 0) {
-        document.getElementById("headline").innerText = "It's my birthday!";
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("content").style.display = "block";
-        clearInterval(x);
-      }
-      //seconds
-    }, 0);
+    //do something later when date is reached
+    if (distance < 0) {
+      document.getElementById("headline").innerText = "It's my birthday!";
+      document.getElementById("countdown").style.display = "none";
+      document.getElementById("content").style.display = "block";
+      clearInterval(x);
+    }
+    //seconds
+  }, 0);
 })();
