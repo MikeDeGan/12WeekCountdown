e5b1fe7bff7347eb52455a8ab2cc392a81de864a
Mike DeGan <mikedegan@gmail.com>
2024-04-01 09:00:24 -0400

    Fix date calculation in script.js

--- script.js
+++ script.js
@@ -11,16 +11,16 @@
   const startYear = periodStart.getFullYear();
   const displayStartDate = startMonth + "/" + startDate + "/" + startYear;
 
-  var periodEnd = new Date();
-  periodEnd.setDate(periodStart.getDate() + 83);
+  var periodEnd = new Date(periodStart);
+  periodEnd.setDate(periodStart.getDate() + 84);
   const endMonth = String(periodEnd.getMonth() + 1).padStart(2, "0");
-  const endDate = String(periodEnd.getDate()).padStart(2, "0");
+  const endDate = String(periodEnd.getDate() - 1).padStart(2, "0");
   const endYear = periodEnd.getFullYear();
   const displayEndDate = endMonth + "/" + endDate + "/" + endYear;
 
   const x = setInterval(function () {
     const now = new Date();
-    const daysRemaining = Math.ceil((periodEnd - now) / day);
+    const daysRemaining = Math.ceil((periodEnd - now) / day) - 1;
     const weeksRemaining = Math.floor(daysRemaining / 7);
     const currentWeek = 12 - weeksRemaining;
 
